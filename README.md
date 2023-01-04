f1b375d1db422f3c70d41d8d584c41281a05ce6c
braiidev-pc <braianleonardocano@gmail.com>
2023-01-04 14:09:40 -0300

    :

---
+++ README.md
@@ -0,0 +1,2 @@
+# VirtualDom (miniReact proyect)
+
--- src/virtualDom/init.js
+++ src/virtualDom/init.js
@@ -1,7 +1,7 @@
 import { render } from "./virtualDom.js";
 
 export function init(vdom) {
-  let css = vdom("link", { rel: "stylesheet", href: "src/components/app.css" });
+  let css = vdom("link", { rel: "stylesheet", href: "src/components/bw.css" });
   let v = 0;
   const c = vdom("div", { id: "app" });
   const h = document.head;
