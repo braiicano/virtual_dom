ae954a475102ebad0a5120a4bc5d78510cbc1591
braiidev-pc <braianleonardocano@gmail.com>
2023-01-05 07:21:06 -0300

    none

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { Welcome } from "./components/Welcome.js";
 import { main, render, vdom } from "./virtualDom/virtualDom.js";
 
-export { main, render, vdom };
-export { Welcome };
+const data = [
+  { id: 1, pj: "Naruto", anime: "Naruto" },
+  { id: 2, pj: "Goku", anime: "Dragon Ball" },
+  { id: 3, pj: "Kenshin Himura", anime: "Rurouni Kenshin" },
+  { id: 4, pj: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
+];
+
+// export { main, render, vdom };
+// export { Welcome };
+
